05b3c603f5b673ff018d71ebda8b15e2e656198f
Matt Fewer <mattfewer@hotmail.com>
2018-08-20 20:05:35 +0200

    section 5 complete

--- resources/functions.php
+++ resources/functions.php
@@ -2,6 +2,23 @@
 
 // HELPERS
 
+
+function set_message($msg) {
+    if (!empty($msg)) {
+        $_SESSION['message'] = $msg;
+    } else {
+        $msg = "";
+    }
+}
+
+function display_message() {
+    if (isset($_SESSION['message'])) {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+
+}
+
 function redirect($location) {
     header("Location: $location");
 }
@@ -131,6 +148,48 @@ $product = <<<DELIMETER
 DELIMETER;
 
 echo $product;
+    }
+}
+
+
+function login_user() {
+    if (isset($_POST['submit'])) {
+        $username = escape_string($_POST['username']);
+        $password = escape_string($_POST['password']);
+
+        $query = query("SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'");
+        confirm($query);
+
+        if (mysqli_num_rows($query) == 0){
+            set_message("Your password or username is not correct");
+            redirect("login.php");
+        } else {
+            set_message("Welcome to Admin {$username}");
+            redirect('admin');
+        }
+    }
+}
+
+
+
+function send_message() {
+    if (isset($_POST['submit'])) {
+        $to = "someemail@gmail.com";
+        $from_name = $_POST['name'];
+        $subject = $_POST['subject'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        $headers = "From: {$from_name} {$email}";
+
+        $result = mail($to, $subject, $message, $headers);
+
+        if (!$result) {
+            set_message("Sorry we could not send your message");
+        } else {
+            set_message("Your message has been sent");
+        }
+        redirect("contact.php");
     }
 }
 
