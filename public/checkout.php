8697c0d347d82bf48a1d81465f07d7bad032278f
Matt Fewer <mattfewer@hotmail.com>
2018-08-26 17:45:18 +0200

    section completed but not working correctly

--- public/checkout.php
+++ public/checkout.php
@@ -1,22 +1,7 @@
 <?php require_once("../resources/config.php"); ?>
+<?php require_once("cart.php"); ?>
 
 <?php include(TEMPLATE_FRONT . DS .  "/header.php");?>
-
-
-<?php
-
-    echo $_SESSION['product_1'] = 1;
-
-?>
-
-
-
-
-
-
-
-
-
 
     <!-- Page Content -->
     <div class="container">
@@ -25,7 +10,7 @@
 <!-- /.row -->
 
 <div class="row">
-
+      <h4 class="text-center bg-danger"><?php display_message(); ?></h4>
       <h1>Checkout</h1>
 
 <form action="">
@@ -40,13 +25,7 @@
           </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>apple</td>
-                <td>$23</td>
-                <td>3</td>
-                <td>2</td>
-
-            </tr>
+            <?php cart(); ?>
         </tbody>
     </table>
 </form>
@@ -62,7 +41,11 @@
 
 <tr class="cart-subtotal">
 <th>Items:</th>
-<td><span class="amount">4</span></td>
+<td><span class="amount">
+    <?php
+        echo isset($_SESSION['item_quantity']) ? $_SESSION['item_quantity'] : $_SESSION['item_quantity'] = "0";
+     ?>
+</span></td>
 </tr>
 <tr class="shipping">
 <th>Shipping and Handling</th>
@@ -71,7 +54,12 @@
 
 <tr class="order-total">
 <th>Order Total</th>
-<td><strong><span class="amount">$3444</span></strong> </td>
+<td><strong><span class="amount">&#36;
+
+    <?php
+        echo isset($_SESSION['item_total']) ? $_SESSION['item_total'] : $_SESSION['item_total'] = "0";
+     ?>
+</span></strong> </td>
 </tr>
 
 
